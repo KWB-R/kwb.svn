675b8837e016d413a0d1f70558d0018c5414fb59
mrustl <michael.rustler@kompetenz-wasser.de>
2019-03-01 23:26:14 +0100

    check in the last hack

--- R/read_logfiles.R
+++ R/read_logfiles.R
@@ -165,6 +165,34 @@ default_repo <- function(root_dir = "svn/kwb",
 
 }
 
+#' Default SVN Repository RScripts
+#' @export
+
+default_rscripts <- function() {
+  
+  default_repo(root_dir = "svn/kwb/R_Development/trunk/RScripts")
+}
+
+#' Default SVN Repository RScripts Paths
+#'
+#' @export
+
+get_rscript_paths <- function() {
+  
+  cmd <- sprintf("svn ls -R %s", 
+                 default_rscripts()
+                 )
+  
+  paths <- shell(cmd = cmd,intern = TRUE) 
+  
+  paths %>% 
+  stringr::str_subset("/$", negate = TRUE) %>%  
+  stringr::str_subset("\\.[rR]([mM][dD])?$") %>% 
+  stringr::str_subset("\\.$", negate = TRUE)
+}
+
+
+
 # getRevisionInfo---------------------------------------------------------------
 #' Get Revision Information
 #'
@@ -290,3 +318,126 @@ readSizeFiles <- function (fDir,
   x$size <- x$size[order(x$size$revision),]
   return(x)
 }
+
+#' Read SVN History For Files
+#'
+#' @param file_paths relative paths to files (relative to repo path)
+#' @param repo path to repository (default: default_repo())
+#' @param tdir target directory (default: tempdir())
+#' @param dbg debug (default: TRUE)
+#' @return writes history files to target directory and returns target directory
+#' @export
+#' @importFrom fs dir_exists dir_create
+read_files_history <- function(file_paths = get_rscript_paths(), 
+                               repo = default_rscripts(), 
+                               tdir = tempdir(), dbg = TRUE) {
+
+  target_dirs <- file.path(tdir, unique(dirname(file_paths)))
+  
+  fs::dir_create(target_dirs[! file.exists(target_dirs)], recursive = TRUE)
+
+sapply(file_paths, FUN = function(file_path) {  
+fpath <- file.path(repo, file_path)
+
+
+cmd <- sprintf("svn log --diff %s > \"%s\"", 
+               file.path(repo, file_path),
+               file.path(tdir, file_path)
+               )
+
+msg <- sprintf("Get '%s' from repo %s and export to %s",  
+               file_path, 
+               repo,
+               tdir)
+
+if (dbg) cat(msg)
+shell(cmd = cmd )
+if (dbg) cat("Done!\n") 
+})
+tdir
+}
+
+#' Read Histories
+#'
+#' @param history_dir directory with history files (as created with 
+# 'read_files_history() )
+#' @return data.frame with columns
+#' @export
+#' @importFrom rlang .data
+#' @importFrom dplyr mutate bind_rows 
+#' @importFrom stringr str_detect str_subset str_remove str_split
+#' @importFrom fs dir_info
+
+read_histories <- function(history_dir) {
+  
+  history_paths <- fs::dir_info(history_dir,
+                                recursive = TRUE, 
+                                all = FALSE, 
+                                type = "file")$path
+  
+  
+  result <- lapply(history_paths, function(history_path) {
+    print(history_path)
+
+    history_script <- readLines(history_path)
+    
+    
+    author_start <- which(stringr::str_detect(history_script, 
+                                              pattern = "^r[0-9]{4} \\| "))
+
+    start <- which(stringr::str_detect(history_script, pattern = "^==========="))
+    
+    end <- c(which(stringr::str_detect(history_script, 
+                                       pattern = "^-------------"))[-1])
+    
+    
+    if(length(author_start) > length(start)) {
+      n_rep <- length(author_start) - length(start)
+      
+      start <- c(start, rep(author_start[length(author_start)],n_rep))
+    }
+    if(length(author_start) > length(end)) {
+      n_rep <- length(author_start) - length(end)
+      
+      end <- c(end, rep(author_start[length(author_start)],n_rep))
+    }
+    
+    if(length(author_start) < length(end)) {
+      n_drop <- length(end) - length(author_start) 
+      end <- end[1:(length(end)-n_drop)]
+    }
+    
+    hist_log <- stringr::str_subset(history_script, pattern = "^r[0-9]{4} \\| ") %>% 
+      stringr::str_split(pattern = "\\s*\\|\\s*",n = 4, simplify = TRUE)  %>% 
+      as.data.frame()
+    
+    names(hist_log) <- c("revision", "username", "datetime", "lines_comment")
+    
+    hist_log_df <- hist_log %>%  
+      dplyr::mutate(
+        file = basename(history_path),
+        revision = as.integer(stringr::str_remove(.data$revision, "^r")), 
+        datetime = stringr::str_sub(.data$datetime, 1, 25),
+        lines_comment = as.integer(stringr::str_remove(.data$lines_comment, "\\slines$")),
+        lines_start = start, 
+        lines_end = end, 
+        lines_added = NA_integer_,
+        lines_deleted = NA_integer_)
+    
+    for(i in seq_len(nrow(hist_log_df))) {
+     sel_lines <- history_script[hist_log_df$lines_start[i]:hist_log_df$lines_end[i]]
+     added <- sum(stringr::str_detect(sel_lines, pattern = "^+"))
+     deleted <- sum(stringr::str_detect(sel_lines, pattern = "^-"))
+                    
+     hist_log_df$lines_added[i] <- ifelse(added > 0, added, NA_integer_)
+    hist_log_df$lines_deleted[i]  <- ifelse(deleted > 0, deleted, NA_integer_)
+    }
+    
+    hist_log_df
+  
+  })
+
+  dplyr::bind_rows(result)
+
+}
+
